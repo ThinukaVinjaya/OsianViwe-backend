86f7a82aca99f44eff2c664c2d3667dba63458ea
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-07-06 13:29:45 +0530

    add option add new room

--- src/main/java/com/thinuka/osianViwe_hotel/repository/RoomRepository.java
+++ src/main/java/com/thinuka/osianViwe_hotel/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package com.thinuka.osianViwe_hotel.repository;
 
-public interface RoomRepository {
+import com.thinuka.osianViwe_hotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
